e29e5532aeda5359f0627f213e06a1804402d5d7
Souayah-Malek12 <malek.souayah@isimg.tn>
2025-09-27 14:19:34 +0100

    Update dependencies and switch to bcryptjs

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,5 +1,5 @@
 const userModel = require("../models/User");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const JWT = require("jsonwebtoken")
 
 const registreController = async (req, res) => {
